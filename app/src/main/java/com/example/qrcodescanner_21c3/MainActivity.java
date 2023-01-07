754d2137865153c71339ee9eedfb38233326f08e
Dimas Wijaya <dimasgustiwijaya93@gmail.com>
2023-01-07 22:24:14 +0700

    fungsi 5 email new

--- app/src/main/java/com/example/qrcodescanner_21c3/MainActivity.java
+++ app/src/main/java/com/example/qrcodescanner_21c3/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+
     @Override
     protected void onActivityResult (int requestCode,int resultCode,Intent data ){
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
@@ -57,6 +58,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             } else if (Patterns.WEB_URL.matcher(result.getContents()).matches()) {
                 Intent visitUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(result.getContents()));
                 startActivity(visitUrl);
+
+            } else if (Patterns.EMAIL_ADDRESS.matcher(result.getContents()).matches()) {
+                Intent intent = new Intent(Intent.ACTION_SEND, Uri.parse(result.getContents()));
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"raihanadzuhri1@gmail.com"});
+                intent.putExtra(Intent.EXTRA_CC, new String[]{"raihancahbekasi1@gmail.com"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Fungsi Email");
+                intent.putExtra(Intent.EXTRA_TEXT, "Raihan Adzuhri-TI.21.C.3-312110374");
+                try {
+                    startActivity(Intent.createChooser(intent, "Ingin Mengirim Email ?"));
+                } catch (android.content.ActivityNotFoundException ex) {
+                }
+            }else if (Patterns.WEB_URL.matcher(result.getContents()).matches()){
+                String geoUri=result.getContents();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(geoUri));
+                //Set Package
+                intent.setPackage("com.google.android.apps.maps");
+
+                //Set Flag
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                startActivity(intent);
             }
             else if (Patterns.PHONE.matcher(result.getContents()).matches()) {
                 String telp = String.valueOf(result.getContents());
@@ -68,19 +91,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 } catch (android.content.ActivityNotFoundException ex) {
                     Toast.makeText(MainActivity.this, "no phone apk installed.", Toast.LENGTH_LONG);
                 }
-            }else if (Patterns.EMAIL_ADDRESS.matcher(result.getContents()).matches()) {
-                Intent intent = new Intent(Intent.ACTION_SEND, Uri.parse(result.getContents()));
-                intent.setType("text/plain");
-                intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"dimasgustiwijaya93@gmail.com"});
-                intent.putExtra(Intent.EXTRA_CC, new String[] {"guntoroagun@gmail.com"});
-                intent.putExtra(Intent.EXTRA_SUBJECT, "Fungsi Email");
-                intent.putExtra(Intent.EXTRA_TEXT, "Dimas G. Wijaya - TI.21.C3 - 312110176");
-                try {
-                    startActivity(Intent.createChooser(intent, "Ingin Mengirim Email ?"));
-                } catch (android.content.ActivityNotFoundException ex) {
-                    //do something else
-                }
-
             }
             else {
                 try {
@@ -95,22 +105,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Toast.makeText(this, result.getContents(),
                             Toast.LENGTH_LONG).show();
                 }
-            } }{
-            try {
-                String geoUri=result.getContents();
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(geoUri));
-                //Set Package
-                intent.setPackage("com.google.android.apps.maps");
+            } }
 
-                //Set Flag
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-                startActivity(intent);
-            }finally {
-
-            }
-
-        }  {
+        else{
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
